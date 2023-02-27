4d65e4a6eb83cb4592d54b0d15683db5833e335f
gardm <86927064+gardm1@users.noreply.github.com>
2023-02-27 22:55:36 +0100

    Add files via upload

---
+++ PuTTY/puttyping.bat
@@ -0,0 +1,42 @@
+@echo off
+
+set server=%1
+set session=%2
+set countdown=5
+
+ping -n 1 %server% > nul
+if %errorlevel% neq 0 (
+    goto :labela
+) else (
+    goto :labelb
+)
+
+:labela
+echo [%server%] %session% is offline
+echo IP %server% not found!
+goto :end
+
+:labelb
+echo [%server%] %session% is online
+set /p confirm="Confirm and connect to your session? (y/n) "
+if /i "%confirm%" equ "y" (
+    echo Connecting to your session...
+    putty.exe -load %session%
+) else (
+    echo Not connecting to session.
+)
+goto :end
+
+:: Can also just change end with pause and exit /b 0
+
+:end
+echo Application will automatically close in %countdown%
+goto :loop
+
+:loop
+echo %countdown%
+set /a countdown-=1
+timeout /t 1 /nobreak > nul
+if %countdown% gtr 0 goto :loop
+
+exit /b 0
