0cc8afb08ac711bfac745b3e1e915defa448f91a
gardm <86927064+gardm1@users.noreply.github.com>
2023-02-27 13:11:02 +0100

    Add files via upload

---
+++ PuTTY/puttyping.bat
@@ -0,0 +1,32 @@
+@echo off
+
+set server=%1
+set session=%2
+
+ping -n 1 %server% > nul
+if %errorlevel% neq 0 goto :labela
+goto :labelb
+
+:labela
+echo [%server%] %session% is offline...
+echo IP %server% not found!
+pause
+goto :end
+
+:labelb
+echo [%server%] %session% is online...
+set /p confirm="Confirm and connect to your session? (y/n) "
+if /i "%confirm%" neq "y" goto :decline
+echo Connecting to your session...
+putty.exe -load %session%
+pause
+goto :end
+
+:decline
+echo Not connecting to session.
+pause
+goto :end
+
+:end
+exit /b 0
+
