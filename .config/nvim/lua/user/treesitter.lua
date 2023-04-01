69b5264c93b67ec0a605dbc0706797aadc900949
gardm <86927064+gardm1@users.noreply.github.com>
2023-04-01 18:38:34 +0200

    Update treesitter.lua

--- .config/nvim/lua/user/treesitter.lua
+++ .config/nvim/lua/user/treesitter.lua
@@ -4,7 +4,7 @@ if not status_ok then
 end
 
 configs.setup({
-  ensure_installed = { "c", "cpp", "go" }, -- one of "all" or a list of languages
+  ensure_installed = { "c", "cpp", "go", "lua" }, -- one of "all" or a list of languages
         highlight = {
                 enable = true, -- false will disable the whole extension
         },
