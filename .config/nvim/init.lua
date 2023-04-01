e978a9e6a62b5a8d3be059953c8bfe9e1b2f03d4
gardm <86927064+gardm1@users.noreply.github.com>
2023-04-01 16:53:59 +0200

    Create init.lua

---
+++ .config/nvim/init.lua
@@ -0,0 +1,11 @@
+require("user.options")
+require("user.keymaps")
+require("user.plugins")
+
+require("user.autopairs")
+
+require("user.cmp")
+require("user.treesitter")
+require("user.lsp")
+
+vim.cmd.colorscheme 'badwolf'
